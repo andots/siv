314f0fff089517850f2ad76e3fdcdeabb5ddc548
andots <18536+andots@users.noreply.github.com>
2024-02-04 04:45:20 +0900

    wip: styling custom titlebar

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,34 +1,43 @@
-import { splitProps, type Component, type ComponentProps } from "solid-js";
+import type { Component } from "solid-js";
 
+import { appWindow } from "@tauri-apps/api/window";
 import { MdiWindowMaximize, MdiWindowClose, MdiWindowMinimize } from "solid-iconify/mdi";
 
 import { Button } from "~/components/ui/button";
 
 import type { IconTypes } from "solid-iconify";
 
-type Props = { icon: IconTypes } & ComponentProps<"div">;
+type Props = { icon: IconTypes; onClick: () => void };
 
 const TitleBarButton: Component<Props> = (props) => {
-  const [, divProps] = splitProps(props, ["icon"]);
-
   return (
-    <div class="inline-flex justify-center items-center w-[30px] h-[30px]" {...divProps}>
-      <Button variant="ghost" size="sm">
-        <props.icon />
-      </Button>
-    </div>
+    <Button variant="rect" class="h-7 w-7" onClick={props.onClick}>
+      <props.icon />
+    </Button>
   );
 };
 
 const TitleBar: Component = () => {
+  const minimize = () => {
+    appWindow.minimize().catch((e) => console.log(e));
+  };
+
+  const maximize = () => {
+    appWindow.maximize().catch((e) => console.log(e));
+  };
+
+  const close = () => {
+    appWindow.close().catch((e) => console.log(e));
+  };
+
   return (
     <div
       data-tauri-drag-region
-      class="h-[30px] select-none flex justify-end fixed top-0 left-0 right-0"
+      class="h-7 select-none flex justify-end fixed top-0 left-0 right-0 bg-neutral-800 text-white"
     >
-      <TitleBarButton icon={MdiWindowMinimize} />
-      <TitleBarButton icon={MdiWindowMaximize} />
-      <TitleBarButton icon={MdiWindowClose} />
+      <TitleBarButton icon={MdiWindowMinimize} onClick={() => minimize()} />
+      <TitleBarButton icon={MdiWindowMaximize} onClick={() => maximize()} />
+      <TitleBarButton icon={MdiWindowClose} onClick={() => close()} />
     </div>
   );
 };
