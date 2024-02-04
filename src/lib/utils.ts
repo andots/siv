ea5579475dbab060ab2cc577415349f6714cf14a
andots <18536+andots@users.noreply.github.com>
2024-02-04 10:28:11 +0900

    wip: get all images in target directory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { readDir } from "@tauri-apps/api/fs";
+import { dirname, extname } from "@tauri-apps/api/path";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -6,3 +8,30 @@ import type { ClassValue } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getImagesInDirectory = async (filePath: string) => {
+  const targetDir = await dirname(filePath);
+  const entries = await readDir(targetDir);
+  const images: string[] = [];
+  for (const entry of entries) {
+    const ext = (await extname(entry.path)).toLowerCase();
+    if (
+      entry.name &&
+      (ext === "jpg" ||
+        ext === "jpeg" ||
+        ext === "jpe" ||
+        ext === "jif" ||
+        ext === "jfif" ||
+        ext === "png" ||
+        ext === "apng" ||
+        ext === "avif" ||
+        ext === "svg" ||
+        ext === "webp" ||
+        ext === "gif")
+    ) {
+      images.push(entry.name);
+    }
+  }
+  // order given filePath as head
+  return images;
+};
