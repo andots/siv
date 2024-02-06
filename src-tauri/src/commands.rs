8477eca966e9e4c9be34e4f45715b65ee1645b5a
andots <18536+andots@users.noreply.github.com>
2024-02-07 03:38:10 +0900

    refactor: use WindowBuilder::from_config

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,4 +1,6 @@
-use tauri::Manager;
+use std::path::PathBuf;
+
+use tauri::{Manager, WindowUrl};
 use window_shadows::set_shadow;
 
 #[tauri::command]
@@ -14,20 +16,26 @@ pub async fn create_window(
     label: String,
     path: String,
 ) -> Result<(), String> {
-    let config = app.config().tauri.windows.get(0).unwrap().clone();
-    let window =
-        tauri::window::WindowBuilder::new(&app, &label, tauri::WindowUrl::App(path.into()))
-            .title(config.title)
-            .decorations(config.decorations)
-            .fullscreen(config.fullscreen)
-            .resizable(config.resizable)
-            .inner_size(config.width, config.height)
-            .min_inner_size(
-                config.min_width.unwrap_or(300.),
-                config.min_height.unwrap_or(300.),
-            )
-            .build()
-            .expect("Can't open new window.");
-    set_shadow(&window, true).expect("Window Shadow: Unsupported Platform!");
+    let mut config = app.config().tauri.windows.get(0).unwrap().clone();
+    config.label = label; // set given label
+    config.url = WindowUrl::App(PathBuf::from(path)); // set given path
+    let window = tauri::window::WindowBuilder::from_config(&app, config)
+        .build()
+        .expect("Can't open new window!");
+    window_shadows::set_shadow(&window, true).expect("Window Shadow: Unsupported Platform!");
     Ok(())
 }
+
+// let window =
+//     tauri::window::WindowBuilder::new(&app, &label, tauri::WindowUrl::App(path.into()))
+//         .title(config.title)
+//         .decorations(config.decorations)
+//         .fullscreen(config.fullscreen)
+//         .resizable(config.resizable)
+//         .inner_size(config.width, config.height)
+//         .min_inner_size(
+//             config.min_width.unwrap_or(300.),
+//             config.min_height.unwrap_or(300.),
+//         )
+//         .build()
+//         .expect("Can't open new window.");
