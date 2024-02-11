c9627122f075a261fc3d142c72ac83bf1f86abbf
andots <18536+andots@users.noreply.github.com>
2024-02-11 13:25:55 +0900

    refactor: cleanup code

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -89,57 +89,47 @@ pub fn get_images_in_dir(path: String) -> Result<Vec<String>, String> {
 #[tauri::command]
 pub fn tile_windows(app: tauri::AppHandle, label: String) -> Result<(), String> {
     debug!("tile_windows: invoked {}", &label);
-    let invoked_window = match app.get_window(&label) {
-        Some(v) => v,
-        None => return Err(String::from("Window not found")),
-    };
-    let monitor = invoked_window
-        .current_monitor()
-        .map_err(|err| err.to_string())?;
-    if let Some(monitor) = monitor {
-        if let Some(monitor_name) = monitor.name() {
-            let monitor_width = monitor.size().width;
-            let monitor_height = monitor.size().height;
-
-            // let windows = app.windows();
-            let mut filtered_windows: Vec<Window> = vec![];
-            for (_label, window) in app.windows() {
-                let current = window.current_monitor();
-                match current {
-                    Ok(Some(b)) => {
-                        if let Some(name) = b.name() {
+    if let Some(invoked_window) = app.get_window(&label) {
+        if let Ok(Some(monitor)) = invoked_window.current_monitor() {
+            if let Some(monitor_name) = monitor.name() {
+                let mut filtered_windows: Vec<Window> = vec![];
+                for (_label, window) in app.windows() {
+                    if let Ok(Some(current)) = window.current_monitor() {
+                        if let Some(name) = current.name() {
                             if name == monitor_name {
                                 filtered_windows.push(window)
                             }
                         }
                     }
-                    Ok(None) => {}
-                    Err(_) => {}
+                }
+
+                let window_count = filtered_windows.len();
+                let monitor_width = monitor.size().width;
+                let monitor_height = monitor.size().height;
+                if window_count == 1 {
+                    invoked_window.maximize().map_err(|e| e.to_string())?;
+                } else if window_count >= 2 {
+                    for (i, window) in filtered_windows.iter().enumerate() {
+                        // let windows_taskbar_size = 30;
+                        let size = PhysicalSize {
+                            width: monitor_width / window_count as u32,
+                            height: monitor_height,
+                        };
+                        let pos = PhysicalPosition {
+                            x: size.width * i as u32,
+                            y: 0,
+                        };
+                        // debug!("size: {:?}, pos: {:?}", size, pos);
+                        window.set_size(size).map_err(|e| e.to_string())?;
+                        window.set_position(pos).map_err(|e| e.to_string())?;
+                    }
                 }
             }
-            // let window_count = windows.len();
-            let window_count = filtered_windows.len();
-            if window_count == 1 {
-                invoked_window.maximize().map_err(|e| e.to_string())?;
-            } else if window_count >= 2 {
-                for (i, window) in filtered_windows.iter().enumerate() {
-                    // let windows_taskbar_size = 30;
-                    let size = PhysicalSize {
-                        width: monitor_width / window_count as u32,
-                        height: monitor_height,
-                    };
-                    let pos = PhysicalPosition {
-                        x: size.width * i as u32,
-                        y: 0,
-                    };
-                    // debug!("size: {:?}, pos: {:?}", size, pos);
-                    window.set_size(size).map_err(|e| e.to_string())?;
-                    window.set_position(pos).map_err(|e| e.to_string())?;
-                }
-            }
+        } else {
+            return Err(String::from("No monitor found."));
         }
     } else {
-        return Err(String::from("No monitor found."));
+        return Err(String::from("No window found."));
     }
     Ok(())
 }
@@ -179,18 +169,3 @@ mod tests {
         assert_eq!(result.ok().unwrap().len(), 3);
     }
 }
-
-// let filtered_windows = app.windows().iter().filter(|&w| {
-//     let current = w.1.current_monitor();
-//     match current {
-//         Ok(Some(b)) => {
-//             let mut flag = false;
-//             if let Some(name) = b.name() {
-//                 flag = name == monitor_name;
-//             }
-//             flag
-//         }
-//         Ok(None) => false,
-//         Err(_) => false,
-//     }
-// });
